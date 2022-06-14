7fe247de8f8f6910f3a0c3dbacde2e8f61c2434b
DaniDevelopment98 <105693050+DaniDevelopment98@users.noreply.github.com>
2022-06-14 09:39:53 -0600

    Add files via upload

---
+++ newtonjsonEjemplos/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace newtonjsonEjemplos
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public class Account
+        {
+            public string Name { get; set; }
+            public string Email { get; set; }
+            public DateTime DOB { get; set; }
+        }
+
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Account account = new Account
+            {
+                Name = "John Doe",
+                Email = "john@microsoft.com",
+                DOB = new DateTime(1980, 2, 20, 0, 0, 0, DateTimeKind.Utc),
+            };
+            string json = JsonConvert.SerializeObject(account, Formatting.Indented);
+           
+            TextBlock.Text = json;
+        }
+    }
+}
